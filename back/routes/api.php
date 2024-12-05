f8d79f18f05c1a4621c1e5e54c2bc5949241cd91
Jok3Noname <zach.frechin@gmail.com>
2024-12-06 00:29:43 +0100

    ça marche

--- back/routes/api.php
+++ back/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MoneyController;
+
 Route::middleware(\App\Http\Middleware\DisableCors::class)->get('/', function () {
     return response()->json(['message' => 'CORS disabled']);
 });
@@ -11,3 +13,7 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+Route::post('/addMoney', [MoneyController::class,'addMoney'])->middleware('auth:sanctum');
+Route::get('/getMoney', [MoneyController::class,'getMoney'])->middleware('auth:sanctum');
